6b5736e3f5ffda625067ed2e5671e582df4a7f04
Paolo <paolo.dincau@gmail.com>
2013-04-01 13:02:12 +0200

    change test file

--- test/mock_apn_test.erl
+++ test/mock_apn_test.erl
@@ -5,7 +5,7 @@
 
 connect() ->
         Address = "localhost",
-        Port = 2195,
+        Port = 5555,
 
         Options = [{mode, binary}, {packet, 0}, {active, false}],
         Timeout = 1000,
@@ -18,15 +18,20 @@ push() ->
         
         PayloadLength = erlang:byte_size(?PAYLOAD),
   
-        DeviceToken = "validdevicetokenhere",
-        DeviceTokenNum = list_to_integer(DeviceToken, 16),
-        BinDeviceToken = <<DeviceTokenNum:32/integer-unit:8>>,
-        BinTokenLength = byte_size(BinDeviceToken),
-
-        DeviceToken1 = "invaliddevicetokenhere",
+        DeviceToken1 = "validdevicetokenhere",
         DeviceTokenNum1 = list_to_integer(DeviceToken1, 16),
         BinDeviceToken1 = <<DeviceTokenNum1:32/integer-unit:8>>,
         BinTokenLength1 = byte_size(BinDeviceToken1),
+
+        DeviceToken2 = "invaliddevicetokenhere",
+        DeviceTokenNum2 = list_to_integer(DeviceToken2, 16),
+        BinDeviceToken2 = <<DeviceTokenNum2:32/integer-unit:8>>,
+        BinTokenLength2 = byte_size(BinDeviceToken2),
+
+        DeviceToken3 = "invaliddevicetokenhere",
+        DeviceTokenNum3 = list_to_integer(DeviceToken3, 16),
+        BinDeviceToken3 = <<DeviceTokenNum3:32/integer-unit:8>>,
+        BinTokenLength3 = byte_size(BinDeviceToken3),
 
         Id1 = 1,
         Id2 = 2,
@@ -35,9 +40,9 @@ push() ->
         {MSeconds, Seconds, _} = erlang:now(),
         Expiry = MSeconds * 1000000 + Seconds + 3600*1,
 
-        Packet1 = <<1:8, Id1:32/big, Expiry:32/big, BinTokenLength:16/big, BinDeviceToken/binary, PayloadLength:16/big, ?PAYLOAD/binary>>,
-        Packet2 = <<1:8, Id2:32/big, Expiry:32/big, BinTokenLength1:16/big, BinDeviceToken1/binary, PayloadLength:16/big, ?PAYLOAD/binary>>,
-	Packet3 = <<1:8, Id3:32/big, Expiry:32/big, BinTokenLength1:16/big, BinDeviceToken1/binary, PayloadLength:16/big, ?PAYLOAD/binary>>,
+        Packet1 = <<1:8, Id1:32/big, Expiry:32/big, BinTokenLength1:16/big, BinDeviceToken1/binary, PayloadLength:16/big, ?PAYLOAD/binary>>,
+        Packet2 = <<1:8, Id2:32/big, Expiry:32/big, BinTokenLength2:16/big, BinDeviceToken2/binary, PayloadLength:16/big, ?PAYLOAD/binary>>,
+	Packet3 = <<1:8, Id3:32/big, Expiry:32/big, BinTokenLength3:16/big, BinDeviceToken3/binary, PayloadLength:16/big, ?PAYLOAD/binary>>,
 
         ssl:send(Socket, Packet1),
         ssl:send(Socket, Packet2),
